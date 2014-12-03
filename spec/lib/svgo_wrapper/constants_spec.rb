6d9b69c1ae149241cc4315de295364ed313bfbd8
gdeoliveira <gdeoliveira@tribune.com>
2014-12-03 16:45:34 -0300

    Move supported plugins to constants file.

--- spec/lib/svgo_wrapper/constants_spec.rb
+++ spec/lib/svgo_wrapper/constants_spec.rb
@@ -3,3 +3,9 @@ require "spec_helper"
 describe SvgoWrapper::DEFAULT_TIMEOUT do
   it { is_expected.to be_a(Numeric) }
 end
+
+describe SvgoWrapper::PLUGINS do
+  it "contains safe names" do
+    expect(subject).to all(match(/\A\w+\z/))
+  end
+end
--- spec/lib/svgo_wrapper/plugins_spec.rb
+++
@@ -1,7 +0,0 @@
-require "spec_helper"
-
-describe SvgoWrapper::PLUGINS do
-  it "contains safe names" do
-    expect(subject).to all(match(/\A\w+\z/))
-  end
-end
