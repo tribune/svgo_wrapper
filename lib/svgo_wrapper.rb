6748215d251a6065f1f0c665a42f76f5dab84fb4
gdeoliveira <gdeoliveira@tribune.com>
2014-12-03 11:42:59 -0300

    Add `svgo` tool check code and warning.

--- lib/svgo_wrapper.rb
+++ lib/svgo_wrapper.rb
@@ -1,4 +1,11 @@
+require "svgo_wrapper/svgo_check"
 require "svgo_wrapper/version"
 
 module SvgoWrapper
+  unless svgo_present?
+    warn <<-EOS
+WARNING: Required tool `svgo` not found on the system.
+         Please follow these instructions to install it: https://github.com/svg/svgo#how-to-use
+    EOS
+  end
 end
---
+++ lib/svgo_wrapper/svgo_check.rb
@@ -0,0 +1,8 @@
+module SvgoWrapper
+  class << self
+    def svgo_present?
+      response = `echo "<svg xmlns='http://www.w3.org/2000/svg'></svg>" | svgo -i - -o - 2> /dev/null`
+      !!(response =~ /\A<svg/)
+    end
+  end
+end
