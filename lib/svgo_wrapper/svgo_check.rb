d3c8488e5514783c4142e383d10cb6b3b0135f7d
gdeoliveira <gdeoliveira@tribune.com>
2014-12-03 15:00:48 -0300

    Move warning message.

--- lib/svgo_wrapper/svgo_check.rb
+++ lib/svgo_wrapper/svgo_check.rb
@@ -5,4 +5,15 @@ class SvgoWrapper
       !(response =~ /\A<svg/).nil?
     end
   end
+
+  unless svgo_present?
+    # We will warn the user once if the `svgo` tool isn't present on this system when the gem is required. This check
+    # will not be performed again in the code for performance reasons.
+    # :nocov:
+    warn <<-EOS
+WARNING: Required tool `svgo` not found on the system.
+         Please follow these instructions to install it: https://github.com/svg/svgo#how-to-use
+    EOS
+    # :nocov:
+  end
 end
