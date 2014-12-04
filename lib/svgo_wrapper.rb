ded2f6c6af49080e18cdd9964e1098ca95e699ee
gdeoliveira <gdeoliveira@tribune.com>
2014-12-04 11:49:01 -0300

    Add basic optimization method.

--- lib/svgo_wrapper.rb
+++ lib/svgo_wrapper.rb
@@ -7,10 +7,19 @@ require "svgo_wrapper/version"
 class SvgoWrapper
   attr_reader :disabled_plugins, :enabled_plugins, :timeout
 
-  def initialize(enable: [], disable: [], timeout: DEFAULT_TIMEOUT)
+  def initialize(enable: Set.new, disable: Set.new, timeout: DEFAULT_TIMEOUT)
     self.enabled_plugins = filter_plugins(enable).freeze
     self.disabled_plugins = filter_plugins(disable).freeze
     self.timeout = timeout
+  end
+
+  def optimize_images_data(data)
+    Open4.spawn "svgo -i - -o -",
+                stdin: data,
+                stdout: output = "",
+                stdout_timeout: timeout
+
+    output
   end
 
   def timeout=(value)
@@ -22,6 +31,6 @@ class SvgoWrapper
   attr_writer :disabled_plugins, :enabled_plugins
 
   def filter_plugins(plugins)
-    VALID_PLUGINS & plugins.to_a.map(&:to_sym)
+    VALID_PLUGINS & plugins.map(&:to_sym)
   end
 end
