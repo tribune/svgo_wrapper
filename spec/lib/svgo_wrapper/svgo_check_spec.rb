0fbbb13843520b5812ba2821de75507ae2b61940
gdeoliveira <gdeoliveira@tribune.com>
2014-12-05 18:42:17 -0300

    Complete coverage.

--- spec/lib/svgo_wrapper/svgo_check_spec.rb
+++ spec/lib/svgo_wrapper/svgo_check_spec.rb
@@ -6,4 +6,14 @@ describe SvgoWrapper do
   it "finds the `svgo` tool in the current system" do
     expect(subject.svgo_present?).to be(true)
   end
+
+  context "`svgo` tool does not exist" do
+    before(:each) do
+      allow(Open4).to receive(:spawn).and_raise
+    end
+
+    it "does not find the `svgo` tool in the current system" do
+      expect(subject.svgo_present?).to be(false)
+    end
+  end
 end
